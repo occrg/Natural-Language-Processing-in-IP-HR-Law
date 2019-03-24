5c6102c44a8e83ea4e765184e3f49606060a27e0
Oliver Ryan-George <org.9711@hotmail.co.uk>
2019-03-24 19:58:25 +0000

    document details inserted into ui

--- src/ui.py
+++ src/ui.py
@@ -1,23 +1,39 @@
+import os
+
 import tkinter as tk
 from tkinter import ttk
 
-def populate(frame):
-    for c in range(100):
-        firstEntry = tk.Entry(frame)
-        familyEntry = tk.Entry(frame)
-        jobEntry = tk.Entry(frame)
-        pathEntry = tk.Entry(frame)
-        testCheck = tk.Checkbutton(frame)
-        editButton = tk.Button(frame, text="Confirm Edit")
-        removeButton = tk.Button(frame, text="Remove")
-        openButton = tk.Button(frame, text="Open")
+from lib.filesio import csvFileToDocumentDetails
 
-        firstEntry.grid(row=c, column=0, padx=15, pady=1)
-        familyEntry.grid(row=c, column=1, padx=15, pady=1)
-        jobEntry.grid(row=c, column=2, padx=15, pady=1)
-        pathEntry.grid(row=c, column=3, padx=15, pady=1)
-        testCheck.grid(row=c, column=4, padx=15, pady=1)
-        editButton.grid(row=c, column=5, padx=15, pady=1)
+
+def openPDF(pdfPath):
+    os.system('xdg-open %s' % pdfPath)
+
+def populate(frame, documentDetails):
+    journalOptions = { 'International Journal of Heritage Studies',          \
+        'International Journal of Cultural Property',                        \
+        'Journal of Intellectual Property Law',                              \
+        'Journal of World Intellectual Property', '-' }
+    for c, d in enumerate(documentDetails[:10]):
+        titleEntry = tk.Entry(frame, width=50)
+        titleEntry.insert(0, "%s" % d['title'])
+        journalsVar = tk.StringVar(frame, '%s' % d['journal'])
+        journalEntry = tk.OptionMenu(frame, journalsVar, *journalOptions)
+        dateEntry = tk.Entry(frame, width=10)
+        dateEntry.insert(0, '%s' % d['date'])
+        pathEntry = tk.Label(frame, text="%s" % d['pdfPath'])
+        testVar = tk.IntVar(frame, 1)
+        testCheck = tk.Checkbutton(frame, var=testVar)
+        confirmButton = tk.Button(frame, text="Confirm Changes")
+        removeButton = tk.Button(frame, text="Remove")
+        openButton = tk.Button(frame, text="Open", command=lambda: openPDF(d['pdfPath']))
+
+        titleEntry.grid(row=c, column=0, padx=15, pady=1)
+        journalEntry.grid(row=c, column=1, padx=15, pady=1)
+        dateEntry.grid(row=c, column=2, padx=15, pady=1)
+        testCheck.grid(row=c, column=3, padx=15, pady=1)
+        pathEntry.grid(row=c, column=4, padx=15, pady=1)
+        confirmButton.grid(row=c, column=5, padx=15, pady=1)
         removeButton.grid(row=c, column=6, padx=15, pady=1)
         openButton.grid(row=c, column=7, padx=15, pady=1)
 
@@ -26,19 +42,21 @@ def topFrameLayout(frame):
     titleLabel = tk.Label(frame, text="Title")
     journalLabel = tk.Label(frame, text="Journal")
     dateLabel = tk.Label(frame, text="Date")
+    testLabel = tk.Label(frame, text="Test")
     pathLabel = tk.Label(frame, text="Path")
 
     titleLabel.grid(row=0, column=0, padx=15, pady=5)
     journalLabel.grid(row=0, column=1, padx=15, pady=5)
     dateLabel.grid(row=0, column=2, padx=15, pady=5)
-    pathLabel.grid(row=0, column=3, padx=15, pady=1)
+    testLabel.grid(row=0, column=3, padx=15, pady=1)
+    pathLabel.grid(row=0, column=4, padx=15, pady=1)
 
 def onFrameConfigure(canvas):
     canvas.configure(scrollregion=canvas.bbox("all"))
 
-def middleFrameLayout(frame):
+def middleFrameLayout(frame, documentDetails):
     canvas = tk.Canvas(frame, borderwidth=0, background="blue")
-    inFrame = tk.Frame(canvas, background="brown")
+    inFrame = tk.Frame(canvas)
     vsb = tk.Scrollbar(frame, orient="vertical", command=canvas.yview)
     canvas.configure(yscrollcommand=vsb.set)
 
@@ -48,7 +66,7 @@ def middleFrameLayout(frame):
 
     frame.bind("<Configure>", lambda event, canvas=canvas: onFrameConfigure(canvas))
 
-    populate(inFrame)
+    populate(inFrame, documentDetails)
 
 def bottomFrameLayout(frame):
     addButton = tk.Button(frame, text="Add Document")
@@ -83,11 +101,14 @@ def formTabLayout(tab, documentDetails):
     bottomFrame.grid_propagate(False)
 
     topFrameLayout(topFrame)
-    middleFrameLayout(middleFrame)
+    middleFrameLayout(middleFrame, documentDetails)
     bottomFrameLayout(bottomFrame)
 
 
 def main():
+    path = 'data/documentDetails.csv'
+    documentDetails = csvFileToDocumentDetails(path)
+
     root = tk.Tk()
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
@@ -97,7 +118,6 @@ def main():
     tab1 = ttk.Frame(tab_parent)
     tab2 = ttk.Frame(tab_parent)
 
-    documentDetails = []
 
     formTabLayout(tab1, documentDetails)
 
